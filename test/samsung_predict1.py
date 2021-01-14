d02d9d8d126da24a2610677f51f43c0e6c326360
youngriKIM <lemontleo0311@gmail.com>
2021-01-14 13:37:45 +0900

    2021-01-14

--- test/samsung_predict1.py
+++ test/samsung_predict1.py
@@ -1,3 +1,5 @@
+# predcit1은 액면분할 한 후만 데이터에 넣었음
+
 import numpy as np
 import pandas as pd
 
@@ -9,9 +11,28 @@ df = pd.read_csv('../data/csv/ss_data.csv', index_col=0, header=0, encoding='cp9
 df2 = df.iloc[::-1].reset_index(drop=True)
 # print(df2)  (2400, 14)
 
+print(df2.info())
+
+#   Column  Non-Null Count  Dtype
+# ---  ------  --------------  -----
+#  0   시가      2400 non-null   int64
+#  1   고가      2400 non-null   int64
+#  2   저가      2400 non-null   int64
+#  3   종가      2400 non-null   int64
+#  4   등락률     2400 non-null   float64
+#  5   거래량     2397 non-null   float64
+#  6   금액(백만)  2397 non-null   float64
+#  7   신용비     2400 non-null   float64
+#  8   개인      2400 non-null   int64
+#  9   기관      2400 non-null   int64
+#  10  외인(수량)  2400 non-null   int64
+#  11  외국계     2400 non-null   int64
+#  12  프로그램    2400 non-null   int64
+#  13  외인비     2400 non-null   float64
+
 
 # 상관계수 확인
-'''
+
 print(df2.corr())
 
 import matplotlib.pyplot as plt
@@ -21,8 +42,8 @@ sns.set(font_scale=0.6, font='Malgun Gothic', rc={'axes.unicode_minus':False})
 sns.heatmap(data=df.corr(), square=True, annot=True, cbar=True) 
 plt.show()
 
-사용 할 칼럼 : 시가0 / 고가1 / 저가2 / 종가3 / 등락률4 / 금액6 / 신용비7 / 기관9
-'''
+# 사용 할 칼럼 : 시가0 / 고가1 / 저가2 / 종가3 / 등락률4 / 금액6 / 신용비7 / 기관9
+
 
 # x, y 데이터 지정
 x = df2.iloc[1740:2399, [0,1,2,3,4,6,7,9]]
@@ -70,13 +91,13 @@ x_pred = x_pred.reshape(x_pred.shape[0], x_pred.shape[1], 1)
 # print(x_test.shape)         #(132, 8, 1)
 # print(x_pred.shape)         #(1, 8, 1)
 
-np.save('../data/npy/samsung_x_train.npy', arr=x_train)
-np.save('../data/npy/samsung_y_train.npy', arr=y_train)
-np.save('../data/npy/samsung_x_val.npy', arr=x_val)
-np.save('../data/npy/samsung_y_val.npy', arr=y_val)
-np.save('../data/npy/samsung_x_test.npy', arr=x_test)
-np.save('../data/npy/samsung_y_test.npy', arr=y_test)
-np.save('../data/npy/samsung_x_pred.npy', arr=x_pred)
+# np.save('../data/npy/samsung_x_train.npy', arr=x_train)
+# np.save('../data/npy/samsung_y_train.npy', arr=y_train)
+# np.save('../data/npy/samsung_x_val.npy', arr=x_val)
+# np.save('../data/npy/samsung_y_val.npy', arr=y_val)
+# np.save('../data/npy/samsung_x_test.npy', arr=x_test)
+# np.save('../data/npy/samsung_y_test.npy', arr=y_test)
+# np.save('../data/npy/samsung_x_pred.npy', arr=x_pred)
 
 
 #2. 모델구성
@@ -109,7 +130,7 @@ stop = EarlyStopping(monitor='val_loss', patience=16, mode='min')
 # modelpath = '../data/modelcheckpoint/samsung_{epoch:02d}-{val_loss:.4f}.hdf5'
 # check = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
 
-model.fit(x_train, y_train, epochs=1, batch_size=1, validation_data=(x_val, y_val), verbose=1, callbacks=[stop]) #, check])
+model.fit(x_train, y_train, epochs=500, batch_size=1, validation_data=(x_val, y_val), verbose=1, callbacks=[stop]) #, check])
 
 
 #4. 평가, 예측
@@ -122,7 +143,7 @@ print('1/14일 삼성주식 종가: ', y_pred)
 
 
 #========= 기록용
-# 30
-# mse:  1395805.875
-# mae:  804.3372192382812
-# 1/14일 삼성주식 종가:  [[91486.96]]
+# mse:  1286656.875
+# mae:  825.32763671875
+# 1/14일 삼성주식 종가:  [[90572.59]]
+'''
