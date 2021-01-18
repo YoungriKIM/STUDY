0cd86cfee82ecd5c238dc6a859e588f4f4fe20d4
youngriKIM <lemontleo0311@gmail.com>
2021-01-18 23:09:47 +0900

    2021-01-18

--- test/dacon_0118_train.py
+++ test/dacon_0118_train.py
@@ -3,11 +3,10 @@ import pandas as pd
 
 dataset = pd.read_csv('../data/csv/dacon1/train/train.csv', index_col=None, header=0)
 # print(dataset.shape)
-dataset = dataset.iloc[:,4:]
+dataset = dataset.iloc[:,3:]
 
 # print(dataset.shape)      #(52560, 5)
 dataset = np.array(dataset)      #<class 'pandas.core.frame.DataFrame'>
-# dataset = dataset.valuse
 
 def split_xy(dataset, x_row, y_row):
     x, y = list(), list()
@@ -23,33 +22,56 @@ def split_xy(dataset, x_row, y_row):
         y.append(tmp_y)
     return np.array(x), np.array(y)
 
-x, y = split_xy(dataset, 336, 48)
+x, y = split_xy(dataset, 336, 96)
 
 # print(x)
 # print(y)
-print(x.shape)      #(52177, 336, 5)
-print(y.shape)      #(52177, 48, 5)
+print(x.shape)      #(52129, 336, 5)
+print(y.shape)      #(52129, 96, 5)
+
+x = x.reshape(x.shape[0], 7, int(x.shape[1]/7), x.shape[2])
 y = y.reshape(y.shape[0], y.shape[1]*y.shape[2])
 
-# 모델 구성
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, Input, Dropout, Conv1D, Flatten, MaxPooling1D, LSTM, GRU, LeakyReLU
+print(x.shape)      #(52129, 7, 48, 5)
+print(y.shape)      #(52129, 480)
+
+
+# 테스트 불러오기
+pred_data = pd.read_csv('../data/csv/dacon1/test/0.csv', index_col=None, header=0)
+# print(dataset.shape)
+x_pred = pred_data.iloc[:,3:].values
+print(x_pred.shape)      #(336, 5)
+
+x_pred = x_pred.reshape(1, 7, int(x_pred.shape[0]/7), x_pred.shape[1])
+print(x_pred.shape)      #(1, 7, 48, 5)
+
+
+#2. 모델 구성
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 
 model = Sequential()
-model.add(Conv1D(filters = 16, kernel_size = 7, strides=1, padding = 'same', input_shape = (336,5), activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
-model.add(Dense(1))
-
-
+model.add(Conv2D(filters=10, kernel_size=(2,2), strides=1, padding='same', input_shape=(x.shape[1], x.shape[2], x.shape[3]), activation='relu'))
+model.add(MaxPooling2D(pool_size=4))
+model.add(Flatten())
+model.add(Dense(576))
 
 #3. 컴파일, 핏
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
-model.fit(x, y, epochs=1, batch_size=48, verbose=1)
+model.fit(x, y, epochs=20, batch_size=48, verbose=1)
 
 #4. 평가, 예측
 result = model.evaluate(x, y, batch_size=48)
 print('mse: ', result[0])
 print('mae: ', result[1])
 
-# y_pred = model.predict(x_pred)
+y_pred = model.predict(x_pred)
+# print(y_pred)
+y_pred = y_pred.reshape(96,6)
+
+df = pd.DataFrame(y_pred)
+# df.to_csv('../data/csv/dacon0.csv', sep=',') 
+
+# mse:  9698.716796875
+# mae:  36.95664596557617
