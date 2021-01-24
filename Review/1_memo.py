ec87d4b334c89319af7fedfdf3ee849829be9589
youngriKIM <lemontleo0311@gmail.com>
2021-01-24 20:45:18 +0900

    2021-01-24

--- Review/1_memo.py
+++ Review/1_memo.py
@@ -1,108 +1,22 @@
-import numpy as np
-import pandas as pd
-
 ##=======================Add Td, T-Td and GHI features
 def Add_features(data):
-    
+  c = 243.12
+  b = 17.62
   gamma = (b * (data['T']) / (c + (data['T']))) + np.log(data['RH'] / 100)
-
+  dp = ( c * gamma) / (b - gamma)
+  data.insert(1,'Td',dp)
+  data.insert(1,'T-Td',data['T']-data['Td'])
   data.insert(1,'GHI',data['DNI']+data['DHI'])
   return data
 
-train = Add_features(train)
-X_test = Add_features(X_test)
-
-df_train = train.drop(['Day','Minute'],axis=1)
-df_test  = X_test.drop(['Day','Minute'],axis=1)
-
-column_indices = {name: i for i, name in enumerate(df_train.columns)}
-
-def Add_features(data):
-    data['cos'] = np.cos(np.pi/2 - np.abs(data['Hour']%12 - 6)/6*np.pi/2)
-    data.insert(1,'GHI',data['DNI']*data['cos']+data['DHI'])
-    data.drop(['cos'], axis= 1, inplace = True)
-    return data
-
-def preprocess_data(data, is_train = True):
-    data['cos'] = np.cos(np.pi/2 - np.abs(data['Hour']%12 - 6)/6*np.pi/2)
-    data.insert(1,'GHI',data['DNI']*data['cos']+data['DHI'])
-    temp = data.copy()
-    temp = temp[['Hour','TARGET','GHI','DHI','DNI','WS','RH','T']]
-
-#===================================================================
-# 예측값을 제출 형식에 넣기 (예측한 값 9컬럼에 다 복붙함)
-sub = pd.read_csv('../data/DACON_0126/sample_submission.csv')
-
-for i in range(1,10):
-    column_name = 'q_0.' + str(i)
-    sub.loc[sub.id.str.contains("Day7"), column_name] = y_pred[:,0]
-for i in range(1,10):
-    column_name = 'q_0.' + str(i)
-    sub.loc[sub.id.str.contains("Day8"), column_name] = y_pred[:,1]
-
-sub.to_csv('../data/DACON_0126/submission_0120_1.csv', index=False)
-
-
-#===================================================================
-# 내일!!
-x = []
+  x = []
 for i in quantiles:
-    model = mymodel()
-    filepath_cp = f'../dacon/data/modelcheckpoint/dacon_y1_quantile_{i:.1f}.hdf5'
-    cp = ModelCheckpoint(filepath_cp,save_best_only=True,monitor = 'val_loss')
+    filepath_cp = f'../dacon/data/modelcheckpoint/dacon_06_y1_quantile_{i:.1f}.hdf5'
+    model = load_model(filepath_cp, compile = False)
     model.compile(loss = lambda y_true,y_pred: quantile_loss(i,y_true,y_pred), optimizer = 'adam', metrics = [lambda y,y_pred: quantile_loss(i,y,y_pred)])
-    model.fit(x_train,y1_train,epochs = epochs, batch_size = bs, validation_data = (x_val,y1_val),callbacks = [es,cp,lr])
     pred = pd.DataFrame(model.predict(x_test).round(2))
     x.append(pred)
 df_temp1 = pd.concat(x, axis = 1)
 df_temp1[df_temp1<0] = 0
 num_temp1 = df_temp1.to_numpy()
 submission.loc[submission.id.str.contains("Day7"), "q_0.1":] = num_temp1
-
-x = []
-# 모레!!
-for i in quantiles:
-    model = mymodel()
-    filepath_cp = f'../dacon/data/modelcheckpoint/dacon_y2_quantile_{i:.1f}.hdf5'
-    cp = ModelCheckpoint(filepath_cp,save_best_only=True,monitor = 'val_loss')
-    model.compile(loss = lambda y_true,y_pred: quantile_loss(i,y_true,y_pred), optimizer = 'adam', metrics = [lambda y,y_pred: quantile_loss(i,y,y_pred)])
-    model.fit(x_train,y2_train,epochs = epochs, batch_size = bs, validation_data = (x_val,y2_val),callbacks = [es,cp,lr])
-    pred = pd.DataFrame(model.predict(x_test).round(2))
-    x.append(pred)
-df_temp2 = pd.concat(x, axis = 1)
-df_temp2[df_temp2<0] = 0
-num_temp2 = df_temp2.to_numpy()
-submission.loc[submission.id.str.contains("Day8"), "q_0.1":] = num_temp2
-        
-submission.to_csv('./practice/dacon/data/111.csv', index = False)
-
-#===================================================================
-
-sub = pd.read_csv('../data/DACON_0126/sample_submission.csv')
-
-for i in range(1,10):
-    column_name = 'q_0.' + str(i)
-    sub.loc[sub.id.str.contains("Day7"), column_name] = y_pred[:,0]
-for i in range(1,10):
-    column_name = 'q_0.' + str(i)
-    sub.loc[sub.id.str.contains("Day8"), column_name] = y_pred[:,1]
-
-sub.to_csv('../data/DACON_0126/submission_0120_1.csv', index=False)
-
-
-
-
-def mymodel():
-    model = Sequential()
-    model.add(Conv1D(256,2,padding = 'same', activation = 'relu',input_shape = (1,8)))
-    model.add(Conv1D(128,2,padding = 'same', activation = 'relu'))
-    model.add(Conv1D(64,2,padding = 'same', activation = 'relu'))
-    model.add(Conv1D(32,2,padding = 'same', activation = 'relu'))
-    model.add(Flatten())
-    model.add(Dense(128, activation = 'relu'))
-    model.add(Dense(64, activation = 'relu'))
-    model.add(Dense(32, activation = 'relu'))
-    model.add(Dense(16, activation = 'relu'))
-    model.add(Dense(8, activation = 'relu'))
-    model.add(Dense(1))
-    return model
