7d32568dfc513b8a0b52d90fb05b27fe7e0895ca
YoungriKIM <lemontleo0311@gmail.com>
2021-02-04 16:17:02 +0900

    2021-02-04

--- dacon3/leave_large_0204_2.py
+++ dacon3/leave_large_0204_2.py
@@ -1,8 +1,8 @@
-# 특성이 큰 것만 남긴 데이터를 쓰겠음
+# 특성이 큰 것만 남긴 데이터를 쓰겠음 버전2
 # 참고 : https://dacon.io/competitions/official/235626/codeshare/1624?page=3&dtype=recent&ptype=pub
 
-# from google.colab import drive
-# drive.mount('/content/drive')
+from google.colab import drive
+drive.mount('/content/drive')
 
 import numpy as np
 import pandas as pd
@@ -14,6 +14,7 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, BatchN
 from tensorflow.keras.models import Sequential, Model, load_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.optimizers import Adam
+
 
 # dataset 불러오기
 train = pd.read_csv('/content/drive/MyDrive/colab_data/dacon3/train.csv')
@@ -29,7 +30,7 @@ train2 = train2.reshape(-1, 28, 28, 1)/255
 test2 = test2.reshape(-1, 28, 28, 1)/255
 
 # 특성 작은값 0으로 수렴
-threshold = 0.1
+threshold = 0.3
 train2[train2 < threshold] = 0
 test2[test2 < threshold] = 0
 
@@ -94,12 +95,12 @@ for train_index, valid_index in skf.split(train2, train['digit']) :
 
     redu_lr = ReduceLROnPlateau(patience= 64, verbose=1, factor=0.1)
     stop = EarlyStopping(monitor='val_acc', patience=128, verbose=1, mode='max')
-    mc = ModelCheckpoint(filepath= '/content/drive/MyDrive/colab_data/modelcheckpoint/dacon3/large_0204_1.h5', save_best_only=True, verbose=1)
+    mc = ModelCheckpoint(filepath= '/content/drive/MyDrive/colab_data/modelcheckpoint/dacon3/large_0204_2.h5', save_best_only=True, verbose=1)
 
     history = model.fit_generator(train_generator, epochs=2000, validation_data=(valid_generator), verbose=1, callbacks=[stop, redu_lr, mc])
 
     # 예측
-    model.load_weights('/content/drive/MyDrive/colab_data/modelcheckpoint/dacon3/large_0204_1.h5')
+    model.load_weights('/content/drive/MyDrive/colab_data/modelcheckpoint/dacon3/large_0204_2.h5')
     result += model.predict_generator(test_generator, verbose=True)/40
     
     nth += 1
@@ -112,7 +113,7 @@ for train_index, valid_index in skf.split(train2, train['digit']) :
 sub['digit'] = result.argmax(1)
 print(sub.head())
 
-sub.to_csv('/content/drive/MyDrive/colab_data/dacon3/large_0204_1.csv', index = False)
+sub.to_csv('/content/drive/MyDrive/colab_data/dacon3/large_0204_2.csv', index = False)
 print('======save complete=====')
 
 # =============================================
@@ -123,3 +124,4 @@ print('(ง˙∇˙)ว {오늘 안에 조지고만다!!!]')
 # epochs = 2000 > 3th_0202_2.csv > dacon score : 0.1078431373  이런 trash
 # 03 수정중 > test 부분 imagegenerate 안하고 (n, 28, 28 ,1)로 넣음 > 3th_0202_3 > daconscore : 0.9264705882	
 # 특성 작은 값 0으로 수렴해서 0.1 >  large_0204_1 > dacon score: 0.93627450
+# 특성 작은 값 0으로 수렴해서2  0.3 > large_0204_2 > dacon score: 0.9068627451	
