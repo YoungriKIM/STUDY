73723467f415ad81fea198a5c25d7e932776e015
YoungriKIM <lemontleo0311@gmail.com>
2021-02-04 18:07:47 +0900

    2021-02-04

--- dacon3/self_model_0204_3.py
+++ dacon3/self_model_0204_3.py
@@ -1,5 +1,5 @@
 # adam 불러와서 디폴트 크게 잡고 시작
-# + 튜닝 조금 / 배치랑 드랍아웃 같이 안쓰게 / 이미지증폭 8 >10
+# + 튜닝 조금 / 배치랑 드랍아웃 같이 안쓰게 / 이미지증폭 8 >10 별로> 8
 
 import pandas as pd
 import numpy as np
@@ -9,7 +9,7 @@ warnings.filterwarnings('ignore')
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Model, Sequential, load_model
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPool2D, BatchNormalization, Dropout
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, BatchNormalization, Dropout, MaxPooling2D
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
@@ -19,12 +19,12 @@ train = pd.read_csv('../data/csv/dacon3/train.csv')
 test = pd.read_csv('../data/csv/dacon3/test.csv')
 sub = pd.read_csv('../data/csv/dacon3/submission.csv')
 
-x_origin = train.drop(['id', 'digit', 'letter'], axis=1).values
-all_test = test.drop(['id', 'letter'], axis=1).values
+train2 = train.drop(['id', 'digit', 'letter'], axis=1).values
+test2 = test.drop(['id', 'letter'], axis=1).values
 
 #------- for conv2d
-x_origin = x_origin.reshape(-1, 28, 28, 1)/255
-all_test = all_test.reshape(-1, 28, 28, 1)/255
+train2 = train2.reshape(-1, 28, 28, 1)/255.0
+test2 = test2.reshape(-1, 28, 28, 1)/255.0
 
 # 이미지 증폭 정의
 idg = ImageDataGenerator(height_shift_range=(-1, 1), width_shift_range=(-1,1))
@@ -33,63 +33,75 @@ idg2 = ImageDataGenerator()
 # kfold 정의
 skf = StratifiedKFold(n_splits=32, random_state=311, shuffle=True)
 
-# ====================================================================
-# 모델 구성 --- Conv2D ---
+reLR = ReduceLROnPlateau(patience=100,verbose=1,factor=0.5) #learning rate scheduler
+es = EarlyStopping(patience=160, verbose=1)
 
 val_loss_min = []
 result = 0
 nth = 0
 
-for train_index, valid_index in skf.split(x_origin, train['digit']) :
-    x_train = x_origin[train_index]
-    x_val = x_origin[valid_index]
+for train_index, valid_index in skf.split(train2,train['digit']) :
+    
+    mc = ModelCheckpoint('../data/modelcheckpoint/dacon3/self_0204_3-2.h5',save_best_only=True, verbose=1)
+    
+    x_train = train2[train_index]
+    x_valid = train2[valid_index]    
     y_train = train['digit'][train_index]
-    y_val = train['digit'][valid_index]
-
-    train_generator = idg.flow(x_train, y_train, batch_size=10)
-    valid_generator = idg2.flow(x_val, y_val)
-    test_generator = idg2.flow(all_test, shuffle=False)
-
+    y_valid = train['digit'][valid_index]
+    
+    train_generator = idg.flow(x_train,y_train,batch_size=8)
+    valid_generator = idg2.flow(x_valid,y_valid)
+    test_generator = idg2.flow(test2,shuffle=False)
+    
     model = Sequential()
-    model.add(Conv2D(filters=128, kernel_size=2, strides=1, padding='same', \
-        input_shape=(x_train.shape[1:]), activation='relu'))
-    model.add(MaxPool2D(pool_size=(2,2)))
-    model.add(Conv2D(64, 2, padding='same', activation='relu'))
+    
+    model.add(Conv2D(16,(3,3),activation='relu',input_shape=(28,28,1),padding='same'))
     model.add(BatchNormalization())
-
-    model.add(Conv2D(64, 2, padding='same', activation='relu'))
-    model.add(MaxPool2D(pool_size=(2,2)))
-    model.add(Conv2D(32, 2, padding='same', activation='relu'))
+    model.add(Dropout(0.3))
+    
+    model.add(Conv2D(32,(3,3),activation='relu',padding='same'))
     model.add(BatchNormalization())
-
+    model.add(Conv2D(32,(5,5),activation='relu',padding='same')) 
+    model.add(BatchNormalization())
+    model.add(Conv2D(32,(5,5),activation='relu',padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32,(5,5),activation='relu',padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((3,3)))
+    model.add(Dropout(0.3))
+    
+    model.add(Conv2D(64,(3,3),activation='relu',padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64,(5,5),activation='relu',padding='same')) 
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((3,3)))
+    model.add(Dropout(0.3))
+    
     model.add(Flatten())
 
     model.add(Dense(128,activation='relu'))
     model.add(BatchNormalization())
+    model.add(Dropout(0.3))
     model.add(Dense(64,activation='relu'))
     model.add(BatchNormalization())
-    model.add(Dense(32,activation='relu'))
-    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
 
     model.add(Dense(10,activation='softmax'))
-
-    # ====================================================================
-
-    #3. 컴파일, 훈련
-    model.compile(loss='sparse_categorical_crossentropy',  optimizer=Adam(lr=0.002,epsilon=None), metrics=['acc'])
-
-    redu_lr = ReduceLROnPlateau(patience= 64, verbose=1, factor=0.5)
-    stop = EarlyStopping(monitor='val_acc', patience=128, verbose=1, mode='max')
-    mc = ModelCheckpoint(filepath= '../data/modelcheckpoint/dacon3/self_0204_3.h5', save_best_only=True, verbose=1)
-
-    history = model.fit_generator(train_generator, epochs=2000, validation_data=(valid_generator), verbose=1, callbacks=[stop, redu_lr, mc])
-
-    # 예측
-    model.load_weights('../data/modelcheckpoint/dacon3/self_0204_3.h5')
-    result += model.predict_generator(test_generator, verbose=True)/40
+    
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=0.002,epsilon=None),metrics=['acc'])
+    
+    learning_history = model.fit_generator(train_generator,epochs=2000, validation_data=valid_generator, callbacks=[es,mc,reLR])
+    
+    # predict
+    model.load_weights('../data/modelcheckpoint/dacon3/self_0204_3-2.h5')
+    result += model.predict_generator(test_generator,verbose=True)/40
+    
+    # save val_loss
+    hist = pd.DataFrame(learning_history.history)
+    val_loss_min.append(hist['val_loss'].min())
     
     nth += 1
-    print(nth, '번째 학습')
+    print(nth, '번째 학습을 완료했습니다.')
 
 # ====================================================================
 
@@ -98,11 +110,12 @@ for train_index, valid_index in skf.split(x_origin, train['digit']) :
 sub['digit'] = result.argmax(1)
 print(sub.head())
 
-sub.to_csv('../data/csv/dacon3/self_0204_3.csv', index = False)
+sub.to_csv('../data/csv/dacon3/self_0204_3-2.csv', index = False)
 print('======save complete=====')
 
 # =============================================
 print('(ง˙∇˙)ว {오늘 안에 조지고만다!!!]')
 # =============================================
 # self_0204_2 > dacon score : 0.9117647059	
-# self_0204_3 > ing
+# self_0204_3 > dacon score : 0.89705 에..;
+# self_0204_3-2 > dacon score : 0.9460784314	
